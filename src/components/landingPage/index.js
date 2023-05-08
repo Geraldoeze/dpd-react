91c7fd99c5ee38d610cbd49cf5caf2707317de52
Gerald Ezenagu <egerald344@gmail.com>
2023-05-08 18:53:05 +0100

    resized img

--- src/components/landingPage/index.js
+++ src/components/landingPage/index.js
@@ -1,17 +1,17 @@
-import Main from './main';
-import SubMain from './subMain';
-import Footer from './footer';
-import './index.css'
+import Main from "./main";
+import SubMain from "./subMain";
+import Footer from "./footer";
+
+import "./index.css";
 
 const Dashboard = () => {
-
-    return (
-        <div className="dash">
-        <Main />
-        <SubMain />
-        <Footer />
-        </div>
-    )
-}
+  return (
+    <div className="dash">
+      <Main />
+      <SubMain />
+      <Footer />
+    </div>
+  );
+};
 
 export default Dashboard;
